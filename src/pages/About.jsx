1a996929dd41d6b2efff7a6d1d116afef985629b
Bikram ghosh <git015.bikram.ghosh@gnkhalsa.edu.in>
2023-03-07 22:33:10 +0530

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
   const reverse = true;
 
   return (
-    <div className="bg-[#000] text-white w-[100%] h-[fit-content] sm:p-[2rem] p-[1rem]">
+    <div className="bg-[#000] text-white w-full p-[1rem] md:p-[2rem]">
       <a
         href="/"
         className="text-[#fff] hover:text-[var(--grey)] hover:border-b-2 border-[var(--grey)]"
@@ -21,32 +21,32 @@ const About = () => {
         Back
       </a>
 
-      <div className="relative sm:w-[850px] h-[fit-content] min-h-[600px] m-auto mt-[2rem] text-center flex flex-col gap-3 items-center">
-        <p className="sm:text-[1.2rem] text-[1.4rem] uppercase text-[var(--white)] text-bold mb-4">
+      <div className="relative md:w-[850px] w-full h-auto m-auto mt-[2rem] text-center flex flex-col gap-3 items-center">
+        <p className="md:text-[1.2rem] text-[1.4rem] uppercase text-[var(--white)] text-bold mb-4">
           The precious gems of Life
         </p>
 
         <CardSection data={MotherData} />
 
-        <div className="border__line_style sm:w-[250px] w-[350px] bg-[#fff]" />
+        <div className="border__line_style md:w-[250px] w-[350px] bg-[#fff]" />
 
         <CardSection data={SisterData} reverse={reverse} />
 
-        <div className="border__line_style sm:w-[250px] w-[350px] bg-[#fff]" />
+        <div className="border__line_style md:w-[250px] w-[350px] bg-[#fff]" />
 
         <CardSection data={FriendData} />
 
-        <div className="border__line_style bg-[#fff] sm:w-[250px] w-[350px]" />
+        <div className="border__line_style bg-[#fff] md:w-[250px] w-[350px]" />
 
         <CardSection data={WifeData} reverse={reverse} />
 
-        <div className="border__line_style bg-[#fff] sm:w-[250px] w-[350px]" />
+        <div className="border__line_style bg-[#fff] md:w-[250px] w-[350px]" />
 
         <CardSection data={DaughterData} />
 
-        <div className="border__line_style bg-[#fff] sm:w-[250px] w-[350px]" />
+        <div className="border__line_style bg-[#fff] md:w-[250px] w-[350px]" />
 
-        <p className="sm:w-[500px] sm:text-[1.3rem] text-[#f0e4ce] sm:my-10 w-[410px] my-4">
+        <p className="md:w-[500px] md:text-[1.3rem] text-[#f0e4ce] my-4">
           Dear men, let us work together to empower the women in our lives. Let
           us listen to their voices, support their dreams, and celebrate their
           achievements. Let us stand up against gender inequality,
@@ -64,3 +64,4 @@ const About = () => {
 };
 
 export default About;
+
